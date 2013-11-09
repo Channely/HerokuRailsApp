a4c8f41ad1389c309f5728cab6fb210f90fe75e6
Channely <qijie29896@gmail.com>
2013-11-09 19:27:38 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HerokuRailsApp::Application.config.secret_key_base = '85260314e8d155b1521ef12bf4e70198642d662cc60359fe3c582f836e603d7c9c5ba35b1e065c502c6849eea1fab5ec1c463a9ef0816aeef789ceb0b0f663ed'
